8cfd9516649a094bb73927b65d0b01ea889fdbcc
Timothy Bueno <timbueno@gmail.com>
2013-01-26 16:34:56 -0500

    DataTypes.h file necessary for command datatype

---
+++ Arduino/Other/DataTypes.h
@@ -0,0 +1,11 @@
+////////////////////////////////////////////////////////////////////////////////
+// Command Structure
+struct Command{
+  bool setTime;
+  unsigned long time;
+
+  bool lightStatus;
+  bool r;
+  bool g;
+  bool b;
+};
