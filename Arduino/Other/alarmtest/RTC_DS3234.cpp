bcc044819e27008908c10b10b9f363f5d98229cc
Timothy Bueno <timbueno@gmail.com>
2013-01-25 20:02:39 -0500

    includes test code that aided in getting the Alarms working with the interrupts on the arduino.

---
+++ Arduino/Other/alarmtest/RTC_DS3234.cpp
@@ -0,0 +1,173 @@
+// Code by JeeLabs http://news.jeelabs.org/code/
+// Released to the public domain! Enjoy!
+
+#if ARDUINO < 100
+#include <WProgram.h>
+#else
+#include <Arduino.h>
+#endif
+
+#include <avr/pgmspace.h>
+#include <SPI.h>
+#include "RTClib.h"
+#include "RTC_DS3234.h"
+
+////////////////////////////////////////////////////////////////////////////////
+// RTC_DS3234 implementation
+
+// Registers we use
+const int CONTROL_R = 0x0e;
+const int CONTROL_W = 0x8e;
+const int CONTROL_STATUS_R = 0x0f;
+const int CONTROL_STATUS_W = 0x8f;
+const int SECONDS_R = 0x00;
+const int SECONDS_W = 0x80;
+
+// Bits we use
+const int EOSC = 7;
+const int OSF = 7;
+
+uint8_t RTC_DS3234::begin(void)
+{
+    pinMode(cs_pin,OUTPUT);
+    cs(HIGH);
+    SPI.setBitOrder(MSBFIRST);
+
+    //Ugh!  In order to get this to interop with other SPI devices,
+    //This has to be done in cs()
+    //SPI.setDataMode(SPI_MODE1);
+
+    //Enable oscillator, disable square wave, alarms
+    cs(LOW);
+    SPI.transfer(CONTROL_W);
+    // SPI.transfer(0x0);
+    // SPI.transfer(0x06);
+    SPI.transfer(0x05);
+    cs(HIGH);
+    delay(1);
+
+    //Clear oscilator stop flag, 32kHz pin
+    cs(LOW);
+    SPI.transfer(CONTROL_STATUS_W);
+    SPI.transfer(0x0);
+    cs(HIGH);
+
+    //SPI.setDataMode(SPI_MODE0);
+    delay(1);
+
+    return 1;
+}
+
+void RTC_DS3234::cs(int _value)
+{
+    if(_value == 0)
+        SPI.setDataMode(SPI_MODE1);
+    else
+        SPI.setDataMode(SPI_MODE0);
+    digitalWrite(cs_pin,_value);
+}
+
+void RTC_DS3234::clearflags()
+{
+    cs(LOW);
+    SPI.transfer(CONTROL_STATUS_W);
+    SPI.transfer(0x0);
+    cs(HIGH);
+}
+
+// flags are: A1M1 (seconds), A1M2 (minutes), A1M3 (hour), 
+// A1M4 (day) 0 to enable, 1 to disable, DY/DT (dayofweek == 1/dayofmonth == 0)
+void RTC_DS3234::setA1(uint8_t s, uint8_t mi, uint8_t h, uint8_t d, bool * flags)
+{
+    uint8_t t[4] = {s, mi, h, d};
+    uint8_t i;
+
+    for(i = 0; i <= 3; i++){
+        cs(LOW);
+        SPI.transfer(i + 0x87);
+        if(i == 3){
+            SPI.transfer(bin2bcd(t[3]) | (flags[3] << 7) | (flags[4] << 6));
+        }
+        else{
+            SPI.transfer(bin2bcd(t[i]) | (flags[i] << 7));
+        }
+        cs(HIGH);
+    }
+}
+
+// void DS3234_set_a1(uint8_t pin, uint8_t s, uint8_t mi, uint8_t h, uint8_t d, boolean * flags)
+// {
+//     uint8_t t[4] = { s, mi, h, d };
+//     uint8_t i;
+
+//     for (i = 0; i <= 3; i++) {
+//         digitalWrite(pin, LOW);
+//         SPI.transfer(i + 0x87);
+//         if (i == 3) {
+//             SPI.transfer(dectobcd(t[3]) | (flags[3] << 7) | (flags[4] << 6));
+//         } else
+//             SPI.transfer(dectobcd(t[i]) | (flags[i] << 7));
+//         digitalWrite(pin, HIGH);
+//     }
+// }
+
+// flags are: A2M2 (minutes), A2M3 (hour), A2M4 (day) 0 to enable, 1 to disable, DY/DT (dayofweek == 1/dayofmonth == 0) 
+void RTC_DS3234::setA2(uint8_t mi, uint8_t h, uint8_t d, bool * flags)
+{
+    uint8_t t[3] = {mi, h, d};
+    uint8_t i;
+
+    for(i = 0; i<=2; i++){
+        cs(LOW);
+        SPI.transfer(i + 0x8B);
+        if (i == 2){
+            SPI.transfer(bin2bcd(t[2]) | (flags[2] << 7) | (flags[3] << 6));
+        }
+        else{
+            SPI.transfer(bin2bcd(t[i]) | (flags[i] << 7));
+        }
+        cs(HIGH);
+    }
+}
+
+uint8_t RTC_DS3234::isrunning(void)
+{
+    cs(LOW);
+    SPI.transfer(CONTROL_R);
+    uint8_t ss = SPI.transfer(-1);
+    cs(HIGH);
+    return !(ss & _BV(OSF));
+}
+
+void RTC_DS3234::adjust(const DateTime& dt)
+{
+    cs(LOW);
+    SPI.transfer(SECONDS_W);
+    SPI.transfer(bin2bcd(dt.second()));
+    SPI.transfer(bin2bcd(dt.minute()));
+    SPI.transfer(bin2bcd(dt.hour()));
+    SPI.transfer(bin2bcd(dt.dayOfWeek()));
+    SPI.transfer(bin2bcd(dt.day()));
+    SPI.transfer(bin2bcd(dt.month()));
+    SPI.transfer(bin2bcd(dt.year() - 2000));
+    cs(HIGH);
+
+}
+
+DateTime RTC_DS3234::now()
+{
+    cs(LOW);
+    SPI.transfer(SECONDS_R);
+    uint8_t ss = bcd2bin(SPI.transfer(-1) & 0x7F);
+    uint8_t mm = bcd2bin(SPI.transfer(-1));
+    uint8_t hh = bcd2bin(SPI.transfer(-1));
+    SPI.transfer(-1);
+    uint8_t d = bcd2bin(SPI.transfer(-1));
+    uint8_t m = bcd2bin(SPI.transfer(-1));
+    uint16_t y = bcd2bin(SPI.transfer(-1)) + 2000;
+    cs(HIGH);
+
+    return DateTime (y, m, d, hh, mm, ss);
+}
+
+// vim:ai:cin:sw=4 sts=4 ft=cpp
--- Arduino/Other/alarmtest/alarmtest.ino
+++ Arduino/Other/alarmtest/alarmtest.ino
@@ -1,20 +1,132 @@
-uint8_t bcd2bin (uint8_t val)
-{
-    return val - 6 * (val >> 4);
-}
+#include "SPI.h"
+#include "RTC_DS3234.h"
+#include "RTClib.h"
 
-uint8_t bin2bcd (uint8_t val)
-{
-    return val + 6 * (val / 10);
-}
+int ledPin = 8;
+
+int wirelesspin = 10;
+int ssRTC = 9;
+
+int state = LOW;
+
+volatile const uint8_t incrementstowait = 5;
+volatile uint8_t increments = 0;
+bool doitflag = false;
+
+int test = 0;
+
+RTC_DS3234 RTC(ssRTC);
 
 void setup() {
+	SPI.begin();
+
+	pinMode(ledPin, OUTPUT);
+	digitalWrite(ledPin, LOW);
+	pinMode(wirelesspin, OUTPUT);
+	digitalWrite(wirelesspin, HIGH);
+
+
+	// attachInterrupt(1, HandleInterrupt, LOW);
+	// attachInterrupt(1, blink, LOW);
+	// attachInterrupt(1, doNothing, LOW);
+	// attachInterrupt(1, HandleInterrupt3, LOW);
+
   // put your setup code here, to run once:
 	Serial.begin(9600);
+
+	RTC.begin();
+
+	// bool flags[4] = {1, 1, 1, 0};
+	// RTC.setA2(1, 1, 1, flags);
+
+	bool flags[5] = {1, 1, 1, 1, 1};
+	RTC.setA1(1, 1, 1, 1, flags);
+
+	attachInterrupt(1, HandleInterrupt4, LOW);
+}
+
+void doNothing(){
+	//
+	RTC.clearflags();
+}
+
+void HandleInterrupt(){
+	RTC.clearflags();
+	if(increments == incrementstowait){
+		increments = 1;
+		// doitflag = true;
+		state = !state;
+	}
+	else{
+		increments++;
+		// doitflag = false;
+	}
+}
+
+void HandleInterrupt2(){
+	RTC.clearflags();
+	if(increments == incrementstowait){
+		increments = 1;
+		doitflag = true;
+		// state = !state;
+	}
+	else{
+		increments++;
+		doitflag = false;
+	}
+}
+
+void HandleInterrupt3(){
+	// SPI.begin();
+	RTC.clearflags();
+	// doitflag = !doitflag;
+	state = !state;
+}
+
+void HandleInterrupt4(){
+	// SPI.begin();
+	RTC.clearflags();
+	if(increments == incrementstowait){
+		state = true;
+		increments = 1;
+	}
+	else{
+		increments = increments+1;
+		state = false;
+	}
+}
+
+void blink()
+{
+	SPI.begin();
+	RTC.clearflags();
+	state = !state;
 }
 
 void loop() {
- 	Serial.println(bin2bcd(5));
-	Serial.println(bin2bcd(15));
-	delay(1000);
+
+	const int len = 32;
+    static char buf[len];
+
+    // digitalWrite(ledPin, HIGH);
+    // delay(1000);
+    // digitalWrite(ledPin, LOW);
+    // delay(1000);
+
+	// DateTime now = RTC.now();
+	// Serial.println(now.toString(buf, len));
+	// delay(10);
+
+	digitalWrite(ledPin, state);
+
+	// if(doitflag){
+	// 	state = !state;
+	// 	digitalWrite(ledPin, state);
+	// 	Serial.println("DoItFlag: True");
+	// }else{
+	// 	Serial.println("DoItFlag: False");
+	// };
+	
+	// delay(500);
+
 }
