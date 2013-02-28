a60420a2c6d86ae94616ffd9d3696a62c38cb42b
Timothy Bueno <timbueno@gmail.com>
2013-02-28 17:55:16 -0500

    added location awareness api access as well as improved edge cases surrounding cities with multiple word names i.e. 'North Canton'

--- RaspberryPi/Weather/WunderAPI.py
+++ RaspberryPi/Weather/WunderAPI.py
@@ -4,20 +4,21 @@
 # http://www.wunderground.com/weather/api/d/docs?d=autocomplete-api
 
 import requests
+import string
 from weatherobjects import HourInformation
 
 # API Superclass
 class WunderAPI(object):
-	def __init__(self, wunderkey='d4a1c5b362656ad6'):
-		self.wunderkey = wunderkey
+	def __init__(self, key='d4a1c5b362656ad6'):
+		self.key = key
 		self.list = []
 
 	# Get hourly weather data from Wunderground
 	# given a city and an abreviated state
 	#
 	# Returns: complete json data
-	def wunderhourly(self, city, state):
-		dataURL = 'http://api.wunderground.com/api/%s/hourly/q/%s/%s.json' % (self.wunderkey, state, city)
+	def wunderhourly(self, requesturl):
+		dataURL = 'http://api.wunderground.com/api/%s/hourly/q/%s' % (self.key, requesturl)
 		r = requests.get(dataURL)
 		return r.json()
 
@@ -25,8 +26,8 @@ class WunderAPI(object):
 	# Extracts relvent data and builds HourInformation objects
 	#
 	# Returns: List of HourInformation objects
-	def getWunderHourData(self, city, state):
-		data = self.wunderhourly(city, state)
+	def getWunderHourData(self, requesturl):
+		data = self.wunderhourly(requesturl)
 		hourlyList = []
 		# Instantiate objects with relevent information
 		for hour in data['hourly_forecast']:
@@ -36,3 +37,54 @@ class WunderAPI(object):
 												hour['FCTTIME']['epoch'])
 			)
 		self.list = hourlyList
+
+	# Takes Zip Code
+	# Returns dictionary with city and state and country and request url
+	def getCityState(self, zipcode):
+		dataURL = 'http://api.wunderground.com/api/%s/geolookup/q/%s.json' % (self.key, zipcode)
+		r = requests.get(dataURL)
+
+		data = r.json()
+
+		location = 0
+
+		if 'error' not in data['response']:
+			htmlurl = str(data['location']['requesturl'])
+			jsonurl = htmlurl.replace('html','json')
+
+			location = {'city': str(data['location']['city']),
+						'state': str(data['location']['state']),
+						'country': str(data['location']['country_iso3166']),
+						'jsonurl': jsonurl
+						}
+
+		return location
+
+	# Automatically gets the locations of the ip address
+	def getIPLocation(self):
+		dataURL = 'http://api.wunderground.com/api/%s/geolookup/q/autoip.json' % self.key
+		r = requests.get(dataURL)
+
+		data = r.json()
+
+		location = 0
+
+		if 'error' not in data['response']:
+
+			htmlurl = str(data['location']['requesturl'])
+			jsonurl = htmlurl.replace('html','json')
+
+			location = {'city': str(data['location']['city']),
+						'state': str(data['location']['state']),
+						'country': str(data['location']['country_iso3166']),
+						'jsonurl': jsonurl
+						}
+
+		return location
+
+if __name__ == "__main__":
+
+	api = WunderAPI()
+	location = api.getCityState('44720')
+	print location['jsonurl']
+
